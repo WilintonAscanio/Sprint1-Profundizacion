b1c435ec08ad67f0b0a7a133fe3140d985387050
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-01 18:35:08 -0500

    Add images & changes in style.scss

--- Documents/Bootcamp/profundización/Sprint1-Webpack/src/app/scripts/UI.js
+++ Documents/Bootcamp/profundización/Sprint1-Webpack/src/app/scripts/UI.js
@@ -1,13 +1,19 @@
-// import icono from "../assets/images/icono.pn";
-import fondo from "../assets/images/fondo.jpg";
+import icono from "../assets/images/icono.png";
+import aside__image from "../assets/images/hombre.png"
+import icon_lupa from "../assets/images/search.svg"
+import profile2 from "../assets/images/hombre2.png"
+import icon_enviado from "../assets/images/doble-verificacion.png"
+import visto from "../assets/images/visto__azul.png"
+import icon_emoji from "../assets/images/smile.svg"
+import icon_paper from "../assets/images/paperclip.svg"
+import icon_mic from "../assets/images/mic.svg"
 
 
 
 
 // DOM elements
 
-// const iconoImg = document.getElementById('nav__img');
-
+const iconoImg = document.getElementById('nav__img');
 export const navSignIn = document.getElementById('nav--right--SignIn');
 export const formSignIn = document.getElementById('form_login');
 export const formSignUp = document.getElementById('signUp');
@@ -25,8 +31,29 @@ export const signUp__cel = document.getElementById('signUp__cel');
 export const signUp__password = document.getElementById('signUp__password');
 export const signUp__url = document.getElementById('signUp__url');
 export const signUp__phrase = document.getElementById('signUp__phrase');
+export const asideImage = document.getElementById('aside__image');
+export const lupa = document.getElementById('lupa');
+export const profile2__img = document.getElementById('profile2__img')
+export const visto_azul = document.getElementById('visto_azul')
+export const enviado = document.getElementById('enviado');
+export const img__user2 = document.getElementById('img__user2');
+export const search = document.getElementById('search');
+export const emoji = document.getElementById('emoji');
+export const paperclip = document.getElementById('paperclip');
+export const mic = document.getElementById('mic');
 
 
-// iconoImg.src = icono;
-     
+iconoImg.src = icono;
+welcome__figure.src = icono;
+asideImage.src = aside__image;
+lupa.src = icon_lupa;
+profile2__img.src = profile2;
+visto_azul.src = visto
+enviado.src = icon_enviado;
+img__user2.src = profile2;
+search.src = icon_lupa
+emoji.src = icon_emoji;
+paperclip.src = icon_paper
+mic.src = icon_mic;
+
 
