0256bdde99b701110d0012dbb6829d641e508adf
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-06 23:15:20 -0500

    Add changes in messages & add some functions

--- Documents/Bootcamp/profundización/Sprint1-Webpack/src/app/scripts/UI.js
+++ Documents/Bootcamp/profundización/Sprint1-Webpack/src/app/scripts/UI.js
@@ -12,7 +12,8 @@ import icon_camera from "../assets/images/camara.png"
 import icon_edit from "../assets/images/editar.png"
 import icon_confirm from "../assets/images/cheque.png"
 import icon_back_msg from "../assets/images/x.png"
-import { getALLMessages, getAllUsers, getUser, newMessagge, newUser } from "./services.js";
+import icon_send from "../assets/images/send.svg"
+import { getALLMessages, getAllUsers, getMessage, getUser, newMessagge, newUser } from "./services.js";
 
 
 
@@ -61,10 +62,26 @@ export const back__msg = document.getElementById('back_msg');
 export const lupa_msg = document.getElementById('lupa_msg');
 export const cancel_search = document.getElementById('cancel_search');
 export const send_msg = document.getElementById('send_msg');
+export const sendMSg = document.getElementById('sendMSg');
 export const send_msg_date = document.getElementById('send_msg_date');
 export const search_msg = document.getElementById('search_msg');
 export const input_search_msg = document.getElementById('input_search_msg');
 export const iconTop = document.getElementById('iconTop').href = icono;
+export const nameUser = document.getElementById('nameUser');
+export const edit__img = document.getElementById('edit__img');
+export const edit__name = document.getElementById('edit__name');
+export const chat_container = document.getElementById('chat_container');
+export const main = document.getElementById('main');
+export const main_left = document.getElementById('main_left');
+export const main_right = document.getElementById('main_right');
+export const footer__input = document.getElementById('footer__input');
+export const form__footer = document.getElementById('form__footer');
+export const last_message = document.getElementById('last_message');
+export const messages_user = document.getElementById('messages_user')
+export const mainChat = document.getElementById('mainChat')
+export const chatWith = document.getElementById('chatWith')
+export const searchMsg__with = document.getElementById('searchMsg__with')
+export const searchMsg = document.querySelector(".searchMsg__with");
 
 
 
@@ -94,12 +111,16 @@ lupa_msg.src = icon_lupa;
 cancel_search.src = icon_back_msg;
 send_msg.src = icon_enviado;
 send_msg_date.src = icon_enviado;
+// sendMSg.src = icon_send;
+
+
+const date = new Date()
 
 export const addNewUser = async () => {
-    
+
     const user = {
         nombre: signUp__name.value,
-        celular: signUp__cel.value,
+        id: signUp__cel.value,
         contraseña: signUp__password.value,
         imagen: signUp__url.value,
         online: false,
@@ -107,7 +128,124 @@ export const addNewUser = async () => {
         fecha: ""
 
     }
-    console.log(user);
     await newUser(user);
 }
+
+
+export const renderAllUsers = async (id) => {
+    chat_container.innerHTML = ''
+    const response = await getAllUsers();
+    response.forEach(element => {
+        if (element.id != id) {
+            chat_container.innerHTML += `
+        <div class="chat__container"  data-id=${element.id}>
+                    <figure class="chat__figure" data-id=${element.id}>
+                        <img alt="profile__chat"src="${element.imagen}" class="chat__img" id="profile2__img" data-id=${element.id}>
+                    </figure>
+                    <div class="chat__infoUser" data-id=${element.id}>
+                        <p class="chat__name" data-id=${element.id}>${element.nombre} <span data-id=${element.id}>Viernes</span></p>
+                        <p class="chat__message">
+                            <img alt="enviado" src="${icon_enviado}" id="enviado"> <img alt="visto__azul" src="${visto}" class="hidden" id="visto_azul">
+                            <span data-id=${element.id} id="last_message"></span>
+                        </p>
+
+                    </div>
+                </div>
+        
+        `
+
+        }
+
+    })
+
+
+}
+export const renderChat = async (id1, id2) => {
+    main_left.innerHTML = ''
+    main_right.innerHTML = ''
+    main.innerHTML = '';
+
+    const response = await getALLMessages();
+
+    response.forEach(element => {
+        if (element.idUser1 === id1 && element.idUSer2 === id2) {
+            element.mensajes.forEach(msg => {
+                if (msg.sendBy === id1) {
+
+                    main.innerHTML += `
+                        <section class="main__right message">
+                            <div class="main__messages2">
+                                <p>${msg.message}</p>
+                                <p class="message__date">09:28 am</p>
+                            </div>
+                        </section>
+                           
+                        `
+
+                }
+                if (msg.sendBy === id2) {
+                    main.innerHTML += `
+                        <section class="main__left message">
+                                <div class="main__messages">
+                                    <p>${msg.message}</p>
+                                    <p class="message__date">09:27 am</p>
+                                </div>
+                            </section>
+                           
+                        `
+
+                }
+
+            })
+
+        }
+
+    })
+
+
+}
+export const sendMessage = async (id1, id2) => {
+
+    const send = {
+        idUser1: id1,
+        idUSer2: id2,
+        mensajes: [
+            {
+                sendBy: id1,
+                date: date.getDate(),
+                hour: date.getHours(),
+                message: footer__input.value,
+                visto: false
+            }
+        ]
+
+    }
+    await newMessagge(send);
+
+}
+export const renderUser = async (id) => {
+    messages_user.innerHTML = ''
+    const response = await getUser(id);
+    messages_user.innerHTML = `
+        <img src="${response.imagen}"id="img__user2" class="img_user2">
+        <p id="messages__user2__name">${response.nombre} <span>EN LINEA</span></p>
+        `
+
+
+}
+
+// export const renderSearchMessage = async (id, searchTerm ="") => {
+//     let listMsg = []
+//     let datos = await getALLMessages()
+//     datos.forEach(element => {
+//         listMsg.push(element.mensajes)
+//     });
+//     console.log(listMsg);
+//     const messages = searchTerm ? datos.filter((msg) => msg.mensajes.toLowerCase().includes(searchTerm.toLowerCase())):datos;
+//     console.log(datos);
+    
+
+ 
+// }
+// renderSearchMessage()
 
